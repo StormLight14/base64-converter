03568a8ef7df3d984a90f10ae0acacb8e0c13eb3
StormLight14 <stormpower44@protonmail.com>
2023-05-07 21:12:20 -0600

    delete gen folder

--- src-tauri/gen/android/mobile_base64/app/src/main/java/xyz/stormlightcandev/mobile_base64/MainActivity.kt
+++
@@ -1,7 +0,0 @@
-package xyz.stormlightcandev.mobile_base64
-
-import app.tauri.plugin.PluginManager
-
-class MainActivity : TauriActivity() {
-  var pluginManager: PluginManager = PluginManager(this)
-}
